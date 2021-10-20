44063a0563dbd867a8cfb77a1bc793e125ecb46e
Justyna Oleś <justynaoles88@gmail.com>
2021-10-20 13:22:25 +0200

    add styles to meny items

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,10 +2,8 @@ import React from 'react';
 import './menu-item.styles.scss';
 
 const MenuItem = ({title, imageUrl, size}) => (
-    <li 
-        className={`${size} menu-item`}
-        style={{backgroundImage: `url(${imageUrl})`}}
-    >
+    <li className={`${size} menu-item`}>
+        <span className='background-image' style={{backgroundImage: `url(${imageUrl})`}}></span>
         <div className='content'>
             <h2 className='title'>{title}</h2>
             <span className='subtitle'>SHOP NOW</span>
