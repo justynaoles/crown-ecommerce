0cfd470cfe929e9495e8d309633a124f2cab335f
Justyna Oleś <justynaoles88@gmail.com>
2021-10-21 13:57:32 +0200

    add router and basic navigation

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -19,8 +19,8 @@ class Directory extends React.Component {
         return (
             <ul className='directory-menu'>
                 {
-                    this.state.sections.map( ({title, imageUrl, id, size}) => (
-                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+                    this.state.sections.map( ({id, ...props}) => (
+                        <MenuItem key={id} {...props}/>
                     ))
                 }
             </ul>
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import './menu-item.styles.scss';
 
-const MenuItem = ({title, imageUrl, size}) => (
-    <li className={`${size} menu-item`}>
-        <span className='background-image' style={{backgroundImage: `url(${imageUrl})`}}></span>
-        <div className='content'>
-            <h2 className='title'>{title}</h2>
-            <span className='subtitle'>SHOP NOW</span>
-        </div>
+const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => (
+    <li className={`${size} menu-item`} onClick={()=> history.push(`${match.url}${linkUrl}`)}>
+            <span className='background-image' style={{backgroundImage: `url(${imageUrl})`}}></span>
+            <div className='content'>
+                <h2 className='title'>{title}</h2>
+                <span className='subtitle'>SHOP NOW</span>
+            </div>
     </li>
 )
 
-export default MenuItem;
+export default withRouter(MenuItem);
