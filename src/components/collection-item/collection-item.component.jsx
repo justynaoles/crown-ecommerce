e74d151033a8bae7f83c68a2fe05ef395f1dd536
Justyna Oleś <justynaoles88@gmail.com>
2021-11-02 19:35:05 +0100

    restructure data in collection item to pass data as payload later

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -2,22 +2,25 @@ import React from 'react';
 import './collection-item.styles.scss';
 import CustomButton from '../custom-button/custom-button.component';
 
-const CollectionItem = ({name, imageUrl, price}) => (
+const CollectionItem = ({item}) => {
+    const {name, imageUrl, price} = item;
+    return (
 
-    <div className='item-container'>
-        <div className='collection-item'>
-            <span className='image'
-                style={{backgroundImage: `url(${imageUrl})`}}
-            />
-            <div className='collection-footer'>
-                <span className='name'>{name}</span>
-                <span className='price'>{price}</span>
+        <div className='item-container'>
+            <div className='collection-item'>
+                <span className='image'
+                    style={{backgroundImage: `url(${imageUrl})`}}
+                />
+                <div className='collection-footer'>
+                    <span className='name'>{name}</span>
+                    <span className='price'>{price}</span>
+                </div>
             </div>
+    
+            <CustomButton inverted>Add to cart</CustomButton>
+    
         </div>
-
-        <CustomButton inverted>Add to cart</CustomButton>
-
-    </div>
-)
+    )
+}
 
 export default CollectionItem;
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,7 +3,7 @@ import { withRouter } from 'react-router-dom';
 import './menu-item.styles.scss';
 
 const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => (
-    <li className={`${size} menu-item`} onClick={()=> history.push(`${match.url}${linkUrl}`)}>
+    <li className={`${size ? size : ''} menu-item`} onClick={()=> history.push(`${match.url}${linkUrl}`)}>
             <span className='background-image' style={{backgroundImage: `url(${imageUrl})`}}></span>
             <div className='content'>
                 <h2 className='title'>{title}</h2>
