d5dc65b4de68f602e523db3fba61d0564095bb8f
Justyna Oleś <justynaoles88@gmail.com>
2021-11-10 10:55:29 +0100

    add lib redux persist, add cart items to persistor

--- src/redux/root.reducer.js
+++ src/redux/root.reducer.js
@@ -2,9 +2,19 @@ import { combineReducers } from 'redux';
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 import mobileMenuReducer from './mobile-menu/mobile-menu.reducer';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'
 
-export default combineReducers({
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']
+  }
+
+const rootReducers = combineReducers({
     user: userReducer,
     cart: cartReducer,
     mobileMenu: mobileMenuReducer
 })
+
+export default persistReducer(persistConfig, rootReducers);
