d3252be825659172a993479876818ab8588b92f6
Justyna Oleś <justynaoles88@gmail.com>
2021-12-30 21:01:06 +0100

    reloccate fetch data to saga

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,10 +1,39 @@
-import {takeEvery} from 'redux-saga/effects';
+import {takeEvery, call, put} from 'redux-saga/effects';
 import { ShopActionTypes } from './shop.types';
+import { collectionData, firestore } from '../../firebase/firebase.utils';
+import {fetchCollectionsSucces,  fetchCollectionFailure } from './shop.actions';
 
 export function* fetchCollectionAsync(){
 
-    yield console.log('async action here')
+    try {
+
+        const collectionRef = firestore.collection('collection');
+        const snapshot = yield collectionRef.get();
+        const snapshotCollections = snapshot.docs
+        const collectionsMap = yield call(
+            collectionData,
+            snapshotCollections
+        );
+
+        yield put(fetchCollectionsSucces(collectionsMap));
+
+    } catch(error) {
+        yield put(fetchCollectionFailure(error))
+    }
+
+    // collectionRef.get().then((snapShot) => {
+    //     const snapShotCollection = snapShot.docs;
+    //     const collectionMap = collectionData(snapShotCollection);
+
+    //     console.log(collectionMap, 'collection');
+
+    //     dispatch(fetchCollectionsSucces(collectionMap));
+
+    // }).catch(error => {
+    //     dispatch(fetchCollectionFailure(error));
+    // });
 }
+
 
 export function* fetchCollectionStart(){
     yield takeEvery(
